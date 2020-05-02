dcd00ddc5a6d1fb5091d9f092b2b96a6d11de942
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-05-02 08:39:00 +0200

    Refactoring on tests

---
+++ src/DotLiquid.Extras.Tests/InnerJoinTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using static DotLiquid.Extras.Tests.TestUtils;
+
+namespace DotLiquid.Extras.Tests
+{
+    public class InnerJoinTest
+    {
+        [Test]
+        public void Should_perform_inner_join()
+        {
+            var data = new {
+                Table1 = new [] {
+                    new { Id = 1, Field1 = "T1R1" },
+                    new { Id = 2, Field1 = "T1R2" },
+                    new { Id = 3, Field1 = "T1R3" },
+                },
+                Table2 = new [] {
+                    new { Id = 2, Field2 = "T2R2" },
+                    new { Id = 3, Field2 = "T2R3" },
+                    new { Id = 4, Field2 = "T1R4" },
+                }
+            };
+
+            var template = @"
+                {% assign joined = Table1 | inner_join:Table2,'Id' -%}
+                {% for item in joined -%}
+                    {{ item.Field1 }} | {{ item.Field2 }}
+                {% endfor -%}
+            ";
+
+            var expected = @"
+                T1R2 | T2R2
+                T1R3 | T2R3
+            ";
+
+            AssertRender(data, template, expected);
+        }
+    }
+}
---
+++ src/DotLiquid.Extras.Tests/WhereTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace DotLiquid.Extras.Tests
+{
+    public class WhereTest
+    {
+        [Test]
+        public void Should_wilter_by_clause()
+        {
+            var data = new {
+                Table1 = new[] {
+                    new { Id = 1, Category = "dog"},
+                    new { Id = 2, Category = "cat"},
+                    new { Id = 3, Category = "pet"},
+                    new { Id = 4, Category = "cat"},
+                }
+            };
+
+            var template = @"
+                {% assign filtered = Table1 | where:'Category','cat' -%}
+                {% for item in filtered -%}
+                    {{ item.Id }}
+                {% endfor -%}
+            ";
+
+            var expected = @"
+                2
+                4
+            ";
+
+            TestUtils.AssertRender(data, template, expected);
+        }
+    }
+}
